e11a60f71e6a4365921e71a37ac397358e780145
Sangmin In <me@sangmin.in>
2021-05-22 20:56:58 +0900

    add acm for api domain

--- acm.tf
+++ acm.tf
@@ -5,3 +5,11 @@ resource "aws_acm_certificate" "sangminout_certi" {
 
   subject_alternative_names = ["sangmin.in"]
 }
+
+resource "aws_acm_certificate" "junction_sangmin_certi" {
+  provider = "aws.virginia"
+  domain_name = "*.junction.sangmin.in"
+  validation_method = "DNS"
+
+  subject_alternative_names = ["junction.sangmin.in"]
+}
