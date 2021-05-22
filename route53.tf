8e2e9ff350ea5a88c270264f4fce1c0261e157bc
Sangmin In <me@sangmin.in>
2021-05-22 21:00:29 +0900

    add api route53

--- route53.tf
+++ route53.tf
@@ -13,3 +13,15 @@ resource "aws_route53_record" "junction" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "junction_api" {
+  name = "api.junction.sangmin.in"
+  type = "A"
+  zone_id = aws_route53_zone.junction_zone.zone_id
+
+  alias {
+    name = aws_cloudfront_distribution.junction_api_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.junction_api_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
