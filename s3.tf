d9e738621259725e7c0392f38a2d3ab1b954a5a1
Sangmin In <me@sangmin.in>
2021-05-22 18:35:01 +0900

    add sam bucket

--- s3.tf
+++ s3.tf
@@ -24,4 +24,25 @@ POLICY
 
 resource "aws_s3_bucket" "junction_sam_bucket" {
   bucket = "junction-sam-bucket"
+  acl = "private"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service":  "serverlessrepo.amazonaws.com"
+      },
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::junction-sam-bucket/*",
+      "Condition" : {
+        "StringEquals": {
+          "aws:SourceAccount": "273340123446"
+        }
+      }
+    }
+  ]
+}
+POLICY
 }
